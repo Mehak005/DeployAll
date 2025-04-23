4cd62f2464dfbb7f474a20171edcb5453255f18d
mehak <mehakchamp2000@gmail.com>
2025-04-22 22:19:25 -0400

    Fix import order & unregister SW in index.js

--- React.app/src/index.js
+++ React.app/src/index.js
@@ -1,14 +1,7 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.getRegistrations().then(regs =>
-    regs.forEach(r => r.unregister())
-  );
-}
-
+// React.app/src/index.js
 
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-
 import './index.css';
 
 import React from 'react';
@@ -16,10 +9,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// Unregister any service workers (CRA default) so you never get stale bundles
+
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.getRegistrations().then(regs =>
-    regs.forEach(reg => reg.unregister())
+    regs.forEach(r => r.unregister())
   );
 }
 
