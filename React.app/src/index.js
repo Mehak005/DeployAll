96f845958833f01e07b39a639bcd530577d59907
mehak <mehakchamp2000@gmail.com>
2025-04-22 22:10:49 -0400

    Load Bootstrap CSS before index.css and unregister SW

--- React.app/src/index.js
+++ React.app/src/index.js
@@ -1,10 +1,20 @@
+// 1) Bootstrap _first_ so its resets and utility classes load before anything else
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+import './index.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Unregister any service workers (CRA default) so you never get stale bundles
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(regs =>
+    regs.forEach(reg => reg.unregister())
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -13,7 +23,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
